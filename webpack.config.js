c88a45cf1388d8c98823a0fc300baead203582d9
marilianewton <mariliagomesnewton@gmail.com>
2024-08-20 15:19:59 +0100

    Initial commit + main page style

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/index.js"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Starter File",
